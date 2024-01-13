fe8f796e60a6750dfd8d031330793a02e96aac5d
Janne Hakonen <1950698+jhakonen@users.noreply.github.com>
2024-01-13 16:32:08 +0200

    Lisää /etc/current-system-packages

--- roles/nixos/common-programs.nix
+++ roles/nixos/common-programs.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   # Asenna locate ja updatedb komennot, updatedb ajetaan myös kerran päivässä
   # keskiyöllä
@@ -24,4 +24,12 @@
 
   programs.nix-index.enable = true;
   programs.command-not-found.enable = false;
+
+  # Listaa kaikki asennetut paketit polussa /etc/current-system-packages
+  # Lähde: https://www.reddit.com/r/NixOS/comments/fsummx/comment/fm45htj/
+  environment.etc."current-system-packages".text = let
+    packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
+    sortedUnique = builtins.sort builtins.lessThan (lib.unique packages);
+    formatted = builtins.concatStringsSep "\n" sortedUnique;
+  in formatted;
 }
