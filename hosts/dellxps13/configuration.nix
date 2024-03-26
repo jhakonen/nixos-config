70cbfe55c58498432eb9358b7ae377334e2d30a4
Janne Hakonen <1950698+jhakonen@users.noreply.github.com>
2024-03-26 20:14:10 +0200

    Lisää sanakirjat oikeinkirjoituksen tukea varten

--- hosts/dellxps13/configuration.nix
+++ hosts/dellxps13/configuration.nix
@@ -184,6 +184,9 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    aspell
+    aspellDicts.en
+    aspellDicts.fi
     bitwarden
     brave
     cachix
