9d3dac9352dc4049716a1ea2b1edd16dcdfe41e0
Janne Hakonen <1950698+jhakonen@users.noreply.github.com>
2024-03-26 20:10:57 +0200

    Aseta gitin oletushaara

--- roles/home-manager/git.nix
+++ roles/home-manager/git.nix
@@ -14,6 +14,7 @@ in {
       userName = "Janne Hakonen";
       userEmail = "***REMOVED***";
       aliases.l = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      extraConfig.init.defaultBranch = "main";
     };
     ssh = {
       enable = true;
