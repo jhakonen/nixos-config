ad7c25fb817f619ad650b324d6bdc97d8e15c8d6
Janne Hakonen <1950698+jhakonen@users.noreply.github.com>
2024-05-24 00:35:13 +0300

    Päivitä calibre-web kun Calibren tietokanta muuttuu

--- roles/nixos/calibre.nix
+++ roles/nixos/calibre.nix
@@ -1,7 +1,8 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   catalog = config.dep-inject.catalog;
   LOCAL_LIBRARY_PATH = "/var/lib/calibre-library";
+  LOCAL_URL = "http://127.0.0.1:${toString config.services.calibre-web.listen.port}";
 in {
   services = {
     calibre-web = {
@@ -17,12 +18,32 @@ in {
 
     nginx.virtualHosts.${catalog.services.calibre-web.public.domain} = {
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.calibre-web.listen.port}";
+        proxyPass = LOCAL_URL;
         recommendedProxySettings = true;
       };
       # Käytä Let's Encrypt sertifikaattia
       addSSL = true;
       useACMEHost = "jhakonen.com";
+    };
+  };
+
+  systemd.services.calibre-web.serviceConfig.Environment = [
+    "CALIBRE_RECONNECT=1"
+  ];
+
+  systemd.paths.calibre-web-db-watcher = {
+    description = "Päivitä calibre-webin sisältö kun tietokantaa muutetaan Calibresta";
+    wantedBy = [ "multi-user.target" ];
+    pathConfig = {
+      Unit = "calibre-db-reconnect.service";
+      PathModified = "${LOCAL_LIBRARY_PATH}/metadata.db";
+    };
+  };
+
+  systemd.services.calibre-db-reconnect = {
+    description = "Muodosta yhteys Calibren tietokantaan uudelleen";
+    serviceConfig = {
+      ExecStart = "${pkgs.curl}/bin/curl ${LOCAL_URL}/reconnect";
     };
   };
 
