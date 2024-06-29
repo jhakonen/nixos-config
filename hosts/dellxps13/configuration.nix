b37d765f492b10d83ac87eaf66171a8e84e5a12d
Janne Hakonen <1950698+jhakonen@users.noreply.github.com>
2024-06-29 11:37:19 +0300

    Ota Hakuneko master käytöön

--- hosts/dellxps13/configuration.nix
+++ hosts/dellxps13/configuration.nix
@@ -242,6 +242,11 @@ in
     unstable.errands
     gnumake
     (hakuneko.overrideAttrs(attrs: {  # Manga downloader
+      version = "8.3.4";
+      src = pkgs.fetchurl {
+        url = "https://github.com/manga-download/hakuneko/releases/download/nightly-20200705.1/hakuneko-desktop_8.3.4_linux_amd64.deb";
+        sha256 = "sha256-SOmncBVpX+aTkKyZtUGEz3k/McNFLRdPz0EFLMsq4hE=";
+      };
       postFixup = ''
         makeWrapper ${steam-run}/bin/steam-run $out/bin/hakuneko \
           --add-flags $out/lib/hakuneko-desktop/hakuneko \
