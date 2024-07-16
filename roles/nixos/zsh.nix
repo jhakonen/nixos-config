82a0600ed6f910a9971061990a83fe709af3c19e
Janne Hakonen <1950698+jhakonen@users.noreply.github.com>
2024-07-16 12:08:48 +0300

    Ota ZSH käyttöön myös nix-shell:ssä ja muissa vastaavissa

--- roles/nixos/zsh.nix
+++ roles/nixos/zsh.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   # Lisää ZSH valittavien komentotulkkien listaan
   environment.shells = [ pkgs.zsh ];
@@ -9,4 +9,9 @@
 
   # Tämä vaaditaan kun zsh on lisätty environment.shells listalle
   programs.zsh.enable = true;
+
+  # Ota ZSH käyttöön `nix develop`, 'nix shell' ja `nix-shell` tulkeissa
+  programs.zsh.interactiveShellInit = ''
+    ${lib.getExe pkgs.nix-your-shell} zsh | source /dev/stdin
+  '';
 }
