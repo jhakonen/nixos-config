3c3c2c78a678e40eccd01b9ae3be4dab55ec47f5
Janne Hakonen <1950698+jhakonen@users.noreply.github.com>
2024-09-28 16:54:41 +0300

    Mervi: Ota Wayland uudelleen käyttöön ja paranna steam runneria

--- hosts/mervi/configuration.nix
+++ hosts/mervi/configuration.nix
@@ -74,10 +74,9 @@ in
   #services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
-  services.displayManager.defaultSession = "plasmax11";
 
   # Configure keymap in X11
   services.xserver.xkb = {
