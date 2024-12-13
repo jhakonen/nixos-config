a37a4432f6d81a7e55400bf85a9a462616bade17
Janne Hakonen <1950698+jhakonen@users.noreply.github.com>
2024-12-13 12:08:19 +0200

    Poista 'koti rakenna' komennosta rinnakkainen ajo

--- tools/koti/src/rakenna_command.sh
+++ tools/koti/src/rakenna_command.sh
@@ -1,12 +1,3 @@
-function lopeta_tehtavat() {
-  jobs -p | xargs -r kill
-}
-
-function suodata_stderr() {
-  # Tämä tulee ets-komennosta kun se ajetaan taustalla
-  sed 's/error resizing pty: bad address//g'
-}
-
 toiminto="${args[--toiminto]}"
 debug=${args[--debug]}
 
@@ -14,8 +5,6 @@ koneet=()
 eval "koneet=(${args[kone]:-})"
 mapfile -t koneet < <(suodata_koneet "${koneet[@]}")
 
-# Aja nixos-rebuild kullekkin koneelle
-trap lopeta_tehtavat EXIT
 cd /home/jhakonen/nixos-config/public
 for kone in "${koneet[@]}"; do
   komento=("nixos-rebuild" "$toiminto" "--flake" ".#$kone" "--fast")
@@ -39,14 +28,5 @@ for kone in "${koneet[@]}"; do
   apu_komento=("systemd-inhibit" "--who" "nixos-rebuild $kone" "--why" "Rakennetaan $kone konetta")
   apu_komento+=("ets" "-f" "[%T.%L - $kone]")
   apu_komento+=("${komento[@]}")
-  eval "${apu_komento[*]@Q}" 2> >(suodata_stderr >&2) &
-done
-
-# Odota nixos-rebuild komentojen valmistumista
-while [ "$(jobs -p)" != "" ]; do
-  if ! wait -n; then
-    # Jokin tehtävistä epäonnistui, joten poistu ja trapin kautta lopeta muut
-    # ajossa olevat tehtävät
-    exit 1
-  fi
+  eval "${apu_komento[*]@Q}"
 done
