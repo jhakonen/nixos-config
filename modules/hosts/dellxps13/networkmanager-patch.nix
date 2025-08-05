c6e211eb81e9bf54eb75ed26c60f7a477216df0e
Janne Hakonen <1950698+jhakonen@users.noreply.github.com>
2025-08-05 09:46:39 +0300

    Siirrä networkmanagerin nixpkgs versio flaken inputteihin jotta evaluaatio nopeutuisi

--- modules/hosts/dellxps13/networkmanager-patch.nix
+++ modules/hosts/dellxps13/networkmanager-patch.nix
@@ -1,3 +1,4 @@
+{ inputs, ... }:
 {
   # Vanhempi networkmanagerin versio joka ei aiheuta virhettä kun yritää
   # yhdistää langattomaan verkkoon.
@@ -6,16 +7,9 @@
   #   > 802-11-wireless-security.key-mgmt: property is missing
   # Raportoitu bugi:
   #   https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/issues/1688
-  flake.modules.nixos.dellxps13 = { pkgs, ... }: let
-    # Nixpkgs versio osoitteesta:
-    #   https://lazamar.co.uk/nix-versions/?package=networkmanager&version=1.48.10&fullName=networkmanager-1.48.10&keyName=networkmanager&revision=0bd7f95e4588643f2c2d403b38d8a2fe44b0fc73&channel=nixpkgs-unstable#instructions
-    old-pkgs = import (builtins.fetchGit {
-      name = "nm-1.48.10-for-property-fix";
-      url = "https://github.com/NixOS/nixpkgs/";
-      ref = "refs/heads/nixpkgs-unstable";
-      rev = "0bd7f95e4588643f2c2d403b38d8a2fe44b0fc73";
-    }) { system = pkgs.stdenv.system; };
-  in {
-    networking.networkmanager.package = old-pkgs.networkmanager;
+  # Nixpkgs versio osoitteesta:
+  #   https://lazamar.co.uk/nix-versions/?package=networkmanager&version=1.48.10&fullName=networkmanager-1.48.10&keyName=networkmanager&revision=0bd7f95e4588643f2c2d403b38d8a2fe44b0fc73&channel=nixpkgs-unstable#instructions
+  flake.modules.nixos.dellxps13 = { pkgs, ... }: {
+    networking.networkmanager.package = inputs.nixpkgs-for-nm-1-48-10.legacyPackages.${pkgs.stdenv.system}.networkmanager;
   };
 }
