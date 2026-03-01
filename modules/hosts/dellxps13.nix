dbccce2f2e0d2576f7dd632c516b919733641030
Janne Hakonen <1950698+jhakonen@users.noreply.github.com>
2026-03-01 12:50:51 +0200

    Poista tarpeettomia inputteja

--- modules/hosts/dellxps13.nix
+++ modules/hosts/dellxps13.nix
@@ -15,7 +15,6 @@
 
     nixos = { config, lib, modulesPath, pkgs, ... }: let
       super-productivity-latest = pkgs.callPackage ../../packages/super-productivity.nix {};
-      pkgs-mistral-vibe = import inputs.nixpkgs-mistral-vibe { inherit (pkgs.stdenv) system; };
     in {
       nix.package = pkgs.lix;
       # Ota flaket käyttöön
@@ -92,16 +91,14 @@
         # Hallitse verkkoyhteyttä NetworkManagerilla
         enable = true;
 
-        # Vanhempi networkmanagerin versio joka ei aiheuta virhettä kun yritää
+        # Uudempi networkmanagerin versio (1.56.0 tai uudempi) joka ei aiheuta virhettä kun yritää
         # yhdistää langattomaan verkkoon.
         # Komento:
         #   $ nmcli device wifi connect <SSID> password <salasana>
         #   > 802-11-wireless-security.key-mgmt: property is missing
         # Raportoitu bugi:
         #   https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/issues/1688
-        # Nixpkgs versio osoitteesta:
-        #   https://lazamar.co.uk/nix-versions/?package=networkmanager&version=1.48.10&fullName=networkmanager-1.48.10&keyName=networkmanager&revision=0bd7f95e4588643f2c2d403b38d8a2fe44b0fc73&channel=nixpkgs-unstable#instructions
-        package = inputs.nixpkgs-for-nm-1-48-10.legacyPackages.${pkgs.stdenv.system}.networkmanager;
+        package = pkgs.unstable.networkmanager;
 
         # Yhteys GL-iNet reititimeen katkeilee jos sekä WIFI että Ethernet yhteys on
         # päällä samaan aikaan. Kierrä ongelma laittamalla WIFI pois päältä kun
@@ -331,11 +328,11 @@
         trayscale
         zoom-us
 
-        inputs.qjournalctl.packages.${pkgs.stdenv.system}.default
-        inputs.zen-browser.packages.${pkgs.stdenv.system}.default
         #inputs.mypanel.packages.${pkgs.stdenv.system}.default
 
-        pkgs-mistral-vibe.mistral-vibe
+        unstable.mistral-vibe
+        unstable.npins  # 0.4.0 oci container tukea varten
+        nix-prefetch-docker  # npins riippuvuus
       ];
 
       fonts.packages = with pkgs; [
