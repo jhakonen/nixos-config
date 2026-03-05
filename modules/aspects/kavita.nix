45dce7bdda8f85c1a866e8c5563fc1f002677a47
Janne Hakonen <1950698+jhakonen@users.noreply.github.com>
2026-03-05 22:10:59 +0200

    Muunna Kavita palvelu podman kontiksi ja päivitä uusimpaan versioon

--- modules/aspects/kavita.nix
+++ modules/aspects/kavita.nix
@@ -1,11 +1,37 @@
+{ inputs, ... }:
 {
-  den.aspects.kanto.nixos = { config, ... }: {
-    services.kavita = {
-      enable = true;
-      tokenKeyFile = config.age.secrets.kavita-token-key.path;
-      settings.Port = config.catalog.services.kavita.port;
+  den.aspects.kanto.nixos = { config, pkgs, ... }: let
+    imageSource = inputs.kavita-image { inherit pkgs; };
+    inherit (imageSource) image_name image_digest;
+    dataDirRoot = "/var/lib/kavita";
+  in {
+    # Palvelukäyttäjä
+    users.users.kavita = {
+      description = "kavita service user";
+      isSystemUser = true;
+      group = "kavita";
+      uid = 974;
+    };
+    users.groups.kavita.gid = 967;
+
+    # Docker-kontin määritys
+    virtualisation.oci-containers.containers.kavita = {
+      image = "${image_name}@${image_digest}";
+      environment = {
+        PUID = toString config.users.users.kavita.uid;
+        PGID = toString config.users.groups.kavita.gid;
+        TZ = "Europe/Helsinki";
+      };
+      volumes = [
+        "${dataDirRoot}/config:/config"
+        "${dataDirRoot}/data:/data"
+      ];
+      ports = [
+        "${toString config.catalog.services.kavita.port}:5000"
+      ];
     };
 
+    # Reverse proxyn asetukset
     services.nginx = {
       enable = true;
       virtualHosts.${config.catalog.services.kavita.public.domain} = {
@@ -20,10 +46,28 @@
       };
     };
 
-    age.secrets.kavita-token-key = {
-      file = ../../agenix/kavita-token-key.age;
-      owner = config.services.kavita.user;
-      group = config.services.kavita.user;
+    # Varmuuskopiointi
+    #   Käynnistä:
+    #     systemctl start restic-backups-kavita-oma.service
+    #     systemctl start restic-backups-kavita-veli.service
+    #   Snapshotit:
+    #     sudo restic-kavita-oma snapshots
+    #     sudo restic-kavita-veli snapshots
+    my.services.restic.backups = let
+      bConfig = {
+        paths = [ dataDirRoot ];
+        backupPrepareCommand = "systemctl stop podman-kavita.service";
+        backupCleanupCommand = "systemctl start podman-kavita.service";
+      };
+    in {
+      kavita-oma = bConfig // {
+        repository = "rclone:nas-oma:/backups/restic/kavita";
+        timerConfig.OnCalendar = "01:00";
+      };
+      kavita-veli = bConfig // {
+        repository = "rclone:nas-veli:/home/restic/kavita";
+        timerConfig.OnCalendar = "Sat 02:00";
+      };
     };
 
     # Palvelun valvonta
