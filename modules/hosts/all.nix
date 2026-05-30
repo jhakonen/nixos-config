3346e16ee61faa9b122009d754759bf89ad3a639
Janne Hakonen <1950698+jhakonen@users.noreply.github.com>
2026-05-31 00:10:46 +0300

    Päivitä loput inputit

--- modules/hosts/all.nix
+++ modules/hosts/all.nix
@@ -3,5 +3,5 @@
   imports = [ inputs.den.flakeModule ];
   # Ota Home Manager käyttöön
   den.schema.user.classes = lib.mkDefault [ "homeManager" ];
-  den.ctx.user.includes = [ den._.mutual-provider ];
+  den.schema.user.includes = [ den._.mutual-provider ];
 }
